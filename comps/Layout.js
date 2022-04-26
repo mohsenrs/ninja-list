5bc75adfa77e18ce67b54d2864db869f448127be
mohsenrs <mohsen8022@gmail.com>
2022-04-26 12:59:06 +0430

    layout

--- comps/Layout.js
+++ comps/Layout.js
@@ -1,5 +1,14 @@
+import Navbar from './Navbar'
+import Footer from './Footer'
+
 const Layout = ({ children }) => {
-  return <>{children}</>
+  return (
+    <div className='content'>
+      <Navbar />
+      {children}
+      <Footer />
+    </div>
+  )
 }
 
 export default Layout
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,10 @@
-import Footer from '../comps/Footer'
 import Layout from '../comps/Layout'
-import Navbar from '../comps/Navbar'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <Navbar />
       <Component {...pageProps} />
-      <Footer />
     </Layout>
   )
 }
